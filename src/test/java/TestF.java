3c73fa928a63421b9a6d99c4ee7259263ce87d77
Serhiy Yakovlev <ysa.sergey@gmail.com>
2016-01-22 09:53:44 +0200

    Fin

--- src/test/java/TestF.java
+++ src/test/java/TestF.java
@@ -1,28 +1,31 @@
 
-        import java.util.regex.Pattern;
-        import java.util.concurrent.TimeUnit;
-        import org.junit.*;
-        import static org.junit.Assert.*;
-        import static org.hamcrest.CoreMatchers.*;
-        import org.openqa.selenium.*;
-        import org.openqa.selenium.firefox.FirefoxDriver;
-        import org.openqa.selenium.support.ui.Select;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TestF {
-private WebDriver driver;
-private String baseUrl;
-private boolean acceptNextAlert = true;
-private StringBuffer verificationErrors = new StringBuffer();
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
 
-@Before
-public void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         driver = new FirefoxDriver();
         baseUrl = "http://hvnuk.com.ua/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        }
+    }
 
-@Test
-public void test2() throws Exception {
+    @Test
+    public void test2() throws Exception {
         driver.get("https://petition.mkrada.gov.ua/");
         assertEquals("Петиції", driver.findElement(By.cssSelector("h4")).getText());
         assertEquals("Створити петицію", driver.findElement(By.linkText("Створити петицію")).getText());
@@ -51,52 +54,22 @@ public void test2() throws Exception {
         assertEquals("", driver.findElement(By.id("subscrBtn")).getText());
         assertEquals("", driver.findElement(By.id("subscrEMail")).getText());
         driver.findElement(By.id("subscrEMail")).clear();
+        driver.findElement(By.id("subscrEMail")).sendKeys("123");
+        driver.findElement(By.id("subscrBtn")).click();
+        driver.findElement(By.id("subscrEMail")).clear();
         driver.findElement(By.id("subscrEMail")).sendKeys("yds@gmail.com");
         driver.findElement(By.id("subscrBtn")).click();
         assertEquals("", driver.findElement(By.xpath("//input[@value='\nзакрити']")).getText());
         driver.findElement(By.xpath("//input[@value='\nзакрити']")).click();
-        }
+    }
 
-@After
-public void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         driver.quit();
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
-        fail(verificationErrorString);
+            fail(verificationErrorString);
         }
-        }
-
-private boolean isElementPresent(By by) {
-        try {
-        driver.findElement(by);
-        return true;
-        } catch (NoSuchElementException e) {
-        return false;
-        }
-        }
-
-private boolean isAlertPresent() {
-        try {
-        driver.switchTo().alert();
-        return true;
-        } catch (NoAlertPresentException e) {
-        return false;
-        }
-        }
-
-private String closeAlertAndGetItsText() {
-        try {
-        Alert alert = driver.switchTo().alert();
-        String alertText = alert.getText();
-        if (acceptNextAlert) {
-        alert.accept();
-        } else {
-        alert.dismiss();
-        }
-        return alertText;
-        } finally {
-        acceptNextAlert = true;
-        }
-        }
-        }
+    }
+}
 
